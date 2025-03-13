8641d871b4a2b212322f7a53f4827f2c9e155b37
ethanzito <tommys.worker@gmail.com>
2025-03-13 05:26:57 -0300

    Probar objetos pokemon y listado creando un servicio de localStorage

--- src/pokemon.js
+++ src/pokemon.js
@@ -1,47 +1,8 @@
-import {cargarPokemon, cargarPokemones} from './api/pokemon.js';
-import {mapearPokemon, mapearListadoPokemones} from './mapeadores/pokemon.js';
-import { actualizarTextoAyuda, mostrarTotalPokemones } from './ui/general.js';
-import { actualizarTextoIndicePokemones, mostrarListadoPokemones } from './ui/listado.js';
-import mostrarPaginador from './ui/paginador.js';
-import mostrarPokemon from './ui/pokemon.js';
-import obtenerParametrosDeURL from './utilidades/utilidades.js';
+import {cargarPokemon, cargarPokemones} from './servicios/pokemon.js';
 
-async function cambiarPagina(pagina) {
-  const POKEMONES_POR_PAGINA = 20;
-  let paginaActual;
-  let offset;
-  let limit = POKEMONES_POR_PAGINA;
-
-  if (typeof pagina === 'number') {
-    offset = POKEMONES_POR_PAGINA * (pagina - 1);
-    paginaActual = pagina;
-  } else {
-    const parametros = obtenerParametrosDeURL(pagina);
-    offset = parametros.offset;
-    limit = parametros.limit;
-    paginaActual = Math.ceil(parametros.offset / parametros.limit) + 1;
-  }
-
-  actualizarTextoIndicePokemones('Cargando...');
-
-  const listadoPokemones = mapearListadoPokemones(await cargarPokemones(offset, limit));
-
-  mostrarTotalPokemones(listadoPokemones.total);
-  mostrarListadoPokemones(listadoPokemones.nombresPokemones, async (index) => {
-    actualizarTextoAyuda('Cargando...');
-    mostrarPokemon(mapearPokemon(await cargarPokemon(index)));
-  });
-
-  mostrarPaginador(
-    listadoPokemones.total,
-    paginaActual,
-    listadoPokemones.siguienteUrl,
-    listadoPokemones.anteriorUrl,
-    cambiarPagina,
-  );
+export default async function inicializar() {
+  console.log(await cargarPokemon(1));
+  console.log(await cargarPokemones());
 }
 
-export default function inicializar() {
-  return cambiarPagina(1)
-    .catch((e) => console.error(e));
-}
+
---
+++ src/servicios/pokemon.js
@@ -0,0 +1,53 @@
+import {
+	cargarPokemon as cargarPokemonDeApi,
+	cargarPokemones as cargarPokemonesDeApi
+} from '../api/pokemon.js';
+
+import {
+	cargarPokemon as cargarPokemonDeLocalStorage,
+	cargarPokemones as cargarPokemonesDeLocalStorage,
+	guardarPokemon,
+	guardarPokemones,
+} from '../storage/pokemon.js';
+
+import {mapearPokemon, mapearListadoPokemones} from '../mapeadores/pokemon.js';
+
+const LIMITE_POKEMONES = 20;
+
+/**
+ * @param {String} id
+ * @returns {Pokemon}
+ */
+export async function cargarPokemon(id) {
+	if (id === undefined) {
+		throw new Error('Se necesita un identificador para cargar un pokemon')
+	}
+
+	let pokemon;
+
+	try {
+		pokemon = cargarPokemonDeLocalStorage(id);
+	} catch (e) {
+		const pokemonData = await cargarPokemonDeApi(id);
+		pokemon = mapearPokemon(pokemonData);
+		guardarPokemon(id, pokemon);
+	}
+
+	return pokemon;
+}
+
+/**
+ * @param {String} offset
+ * @param {String} limite
+ * @return {ListadoPokemones}
+ */
+export async function cargarPokemones(offset = 0, limite = LIMITE_POKEMONES) {
+	try {
+		return cargarPokemonesDeLocalStorage(offset, limite);
+	} catch (e) {
+		const pokemonesData = await cargarPokemonesDeApi(offset, limite);
+		const pokemones = mapearListadoPokemones(pokemonesData);
+		guardarPokemones(offset, limite, pokemones);
+		return pokemones;
+	}
+}
