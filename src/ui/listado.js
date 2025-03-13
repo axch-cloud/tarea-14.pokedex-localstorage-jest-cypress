98876aeb561a6638bd77f1bbb4c2e5dbc70a8124
ethanzito <tommys.worker@gmail.com>
2025-03-13 05:49:54 -0300

    Utilizar servicio de localStorage en la UI anterior

--- src/ui/listado.js
+++ src/ui/listado.js
@@ -20,12 +20,12 @@ export function mostrarListadoPokemones(nombresPokemones, pokemonSeleccionadoCal
   const $indice = document.querySelector('#indice');
   $indice.innerHTML = '';
 
-  nombresPokemones.forEach((nombre, index) => {
+  nombresPokemones.forEach((nombre) => {
     const $link = document.createElement('a');
     $link.className = 'list-group-item list-group-item-action';
     $link.setAttribute('href', '#');
     $link.textContent = nombre;
-    $link.onclick = () => pokemonSeleccionadoCallback(index + 1);
+    $link.onclick = () => pokemonSeleccionadoCallback(nombre);
     $indice.appendChild($link);
   });
 }
